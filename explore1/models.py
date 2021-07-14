b59e017a97e990edf4d884426d3f0813b16b9db6
abhishekk-7 <2019186@iiitdmj.ac.in>
2021-07-14 12:33:16 +0530

    arrayfield

--- explore1/models.py
+++ explore1/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 
 
 class Explore(models.Model):
     title = models.CharField(max_length=50)
     body = models.TextField(max_length=400)
-    img_link = models.CharField(max_length=200, blank=True)
+    img_link = ArrayField(models.CharField(max_length=200), blank=True)
 
     def __str__(self):
         return self.title
